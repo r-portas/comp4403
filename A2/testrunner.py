ea2ef1d77abd7495ed71f0b9aaaaaeff663a1860
Roy Portas <royportas@gmail.com>
2017-04-27 08:04:34 +1000

    Fixed testrunner

--- A2/testrunner.py
+++ A2/testrunner.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+A test runner for COMP4403 assignments
+
+author = "Roy Portas <royportas@gmail.com>"
+"""
 
 import os
 import subprocess
@@ -6,6 +11,7 @@ import difflib
 import sys
 import platform
 
+# The directories containing your test results
 ERROR_DIR='my-test-errors'
 RESULT_DIR='my-test-results'
 
@@ -15,27 +21,33 @@ CMP_RESULT_DIR='test-pgm/results'
 
 PATH = 'test-pgm'
 FILE_EXTENSION = '.pl0'
-# Double quotes are important, as well as semicolon
 
-SEPARATOR = ':'
+# Items on the classpath
+PATH_ITEMS = ['bin', 'java-cup-11b.jar']
 
-if platform.system() == 'Windows':
-    SEPARATOR = ';'
-
-classpath_items = ['bin', 'java-cup-11b.jar']
-full_path_items = []
-
-for classpath_item in classpath_items:
-    full_path_items.append(os.path.join(os.getcwd(), classpath_item))
-
-CLASSPATH = '"' + SEPARATOR.join(full_path_items) + '"'
-
+# The main java class
 MAIN = 'pl0.PL0_LALR'
 
-def set_class_path():
-    os.environ['CLASSPATH'] = CLASSPATH
+def generate_classpath(path_items, working_dir):
+    """ Generate the classpath for running a PL0 file """
+    # Unix separator
+    separator = ':'
+
+    # if platform.system() == 'Windows':
+    #     # Windows separator
+    #     separator = ';'
+
+    full_path_items = []
+
+    for item in path_items:
+        full_path_items.append(os.path.join(working_dir, item))
+
+    classpath = '"' + separator.join(full_path_items) + '"'
+    return classpath
+
 
 def create_directories():
+    """ Create the directories if they don't exist """
     if not os.path.isdir(ERROR_DIR):
         os.mkdir(ERROR_DIR)
 
@@ -43,6 +55,7 @@ def create_directories():
         os.mkdir(RESULT_DIR)
 
 def get_test_files(path):
+    """ Get a list of the test file """
     files = os.listdir(path)
 
     pl0_files = []
@@ -52,7 +65,8 @@ def get_test_files(path):
             pl0_files.append(f)
 
     return pl0_files
-def run_test(pl0_file, path):
+
+def run_test(pl0_file, classpath, path):
     path_to_pl0_file = os.path.join(os.getcwd(), path, pl0_file)
     result_file = os.path.join(os.getcwd(), RESULT_DIR, 'r-{}'.format(pl0_file))
     error_file = os.path.join(os.getcwd(), ERROR_DIR, 'e-{}'.format(pl0_file))
@@ -60,10 +74,10 @@ def run_test(pl0_file, path):
     with open(result_file, 'w') as result_fd:
         with open(error_file, 'w') as error_fd:
 
-            args = ['java', '-cp', CLASSPATH, MAIN, path_to_pl0_file]
-            print('>>> ' + ' '.join(args))
+            cmd = 'java -cp {} {} {}'.format(classpath, MAIN, path_to_pl0_file)
+            print('>>> ' + cmd)
 
-            subprocess.Popen(' '.join(args),
+            subprocess.call(cmd,
                 stdout=result_fd,
                 stderr=error_fd,
                 shell=True
@@ -101,16 +115,16 @@ def create_html_report(filename, html_tables):
         f.write('</body></html>')
 
 def main():
-    set_class_path()
+    classpath = generate_classpath(PATH_ITEMS, os.getcwd())
     create_directories()
     pl0_files = get_test_files(PATH)
     for pl0_file in pl0_files:
-        run_test(pl0_file, PATH)
+        run_test(pl0_file, classpath, PATH)
 
     html_tables = []
     for pl0_file in pl0_files:
-        html_tables.append(compare_file(os.path.join(os.getcwd(), RESULT_DIR, 'r-' + pl0_file),
-                os.path.join(os.getcwd(), CMP_RESULT_DIR, 'r-' + pl0_file)))
+        html_tables.append(compare_file(os.path.join(RESULT_DIR, 'r-' + pl0_file),
+                os.path.join(CMP_RESULT_DIR, 'r-' + pl0_file)))
 
     create_html_report('output.html', html_tables)
 main()
